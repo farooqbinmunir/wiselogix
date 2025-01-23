dfc0f03ffd3ee64a9b80aa9b94817af4793730a5
Farooq Bin Munir <farooqbinmunir@gmail.com>
2025-01-23 17:05:04 +0500

    Contributers section added as a compnent

--- src/app/_components/Button.jsx
+++ src/app/_components/Button.jsx
@@ -1,8 +1,8 @@
 export default function Button({btnTxt, link = '', cls=""}) {
     return (
         <>
-            {link && <a href={link} className={`fl-button standard_button ${cls}`}><span className={`fl-button-text`}>{btnTxt}</span></a>}
-            {!link && <button className={`fl-button standard_button ${cls}`}><span className={`fl-button-text`}>{btnTxt}</span></button>}        
+            {link && <a href={link} className={`standard_button ${cls}`}><span className={`fl-button-text`}>{btnTxt}</span></a>}
+            {!link && <button className={`standard_button ${cls}`}><span className={`fl-button-text`}>{btnTxt}</span></button>}        
         </>
     );
 }
