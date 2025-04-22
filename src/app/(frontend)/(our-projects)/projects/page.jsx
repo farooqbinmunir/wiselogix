c5bbfe0dae8d7a0775f323a8511607ce17390b37
Farooq Bin Munir <farooqbinmunir@gmail.com>
2025-04-22 16:00:33 +0500

    Projects page made dynamic

--- src/app/(frontend)/(our-projects)/projects/page.jsx
+++ src/app/(frontend)/(our-projects)/projects/page.jsx
@@ -1,11 +1,30 @@
 import Link from 'next/link';
 import BannerGlobal from "../../../_components/BannerGlobal";
+import client from '../../../../../config.js';
 
 export const metadata = {
     title: "Our Projects - Wiselogix Technologies",
     description: "Lorem ipsum dolor sitm...",
 };
-export default function OurProjects(){
+export default async function OurProjects(){
+    let projectsQuery = `{
+        ourProjects {
+            nodes {
+                databaseId
+                title(format: RAW)
+                slug
+                excerpt(format: RAW)
+                content(format: RENDERED)
+                featuredImage {
+                    node {
+                        sourceUrl
+                    }
+                }
+            }
+        }
+    }`;
+
+    const {ourProjects: {nodes: projects}} = await client.request(projectsQuery);
 
     return(
         <>
@@ -26,91 +45,37 @@ export default function OurProjects(){
                     </div>
                     <div className="section_pad projects_section">
                         <div className="section_heading">
-                            <h2>
-                                Explore Our Projects
-                            </h2>
+                            <h2>Explore Our Projects</h2>
                         </div>
                         <div className="projects_item_wrap">
-                            <div className="projects_item">
-                                <div className="projects_img">
-                                    <div className="project_cpt_image"
-                                        style={{"background": "url(/assets/images/Hist-Polochic.jpg) center center no-repeat;background-size:cover"}}>
+                            {projects.map((project, index) => {
+                                const imgURL = project?.featuredImage?.node?.sourceUrl;
+                                console.log("Image URL: ", imgURL);
+                                return (
+                                    <div className="projects_item" key={index}>
+                                        <div className="projects_img">
+                                            <div className="project_cpt_image fbm" style={{
+                                                "background": `url(${project?.featuredImage?.node?.sourceUrl}) center center no-repeat`,
+                                                "backgroundSize": "cover",
+                                                }
+                                            }></div>
+                                        </div>
+                                        <div className="projects_text">
+                                            <h3>
+                                                {project.title}
+                                            </h3>
+                                            <div className="project_description">
+                                                <p>
+                                                    {project.excerpt}
+                                                </p>
+                                            </div>
+                                            <div className="project_readmore">
+                                                <Link className="oxfam_button" href={`/project/${project.slug}`} >Learn more</Link>
+                                            </div>
+                                        </div>
                                     </div>
-                                </div>
-                                <div className="projects_text">
-                                    <h3>
-                                        Our work with women and children
-                                    </h3>
-                                    <div className="project_description">
-                                        <p>
-                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sed tellus venenatis, luctus leo sit amet, mollis risus. Aenean elementum quis nunc eu auctor. Vivamus elementum sapien a lobortis rhoncus.
-                                        </p>
-                                    </div>
-                                    <div className="project_readmore">
-                                        <Link className="oxfam_button" href="#" >Learn more</Link>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="projects_item">
-                                <div className="projects_img">
-                                    <div className="project_cpt_image"
-                                        style={{"background": "url(/assets/images/South-Sudan_education.png) center center no-repeat;background-size:cover"}}>
-                                    </div>
-                                </div>
-                                <div className="projects_text">
-                                    <h3>
-                                        Championing Education for the Girl Child
-                                    </h3>
-                                    <div className="project_description">
-                                        <p>
-                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sed tellus venenatis, luctus leo sit amet, mollis risus. Aenean elementum quis nunc eu auctor. Vivamus elementum sapien a lobortis rhoncus.
-                                        </p>
-                                    </div>
-                                    <div className="project_readmore">
-                                        <Link className="oxfam_button" href="#" >Learn more</Link>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="projects_item">
-                                <div className="projects_img">
-                                    <div className="project_cpt_image"
-                                        style={{"background": "url(/assets/images/InuruID-Indonesia.jpg) center center no-repeat;background-size:cover"}}>
-                                    </div>
-                                </div>
-                                <div className="projects_text">
-                                    <h3>
-                                        Assisting widows and the marginalized
-                                    </h3>
-                                    <div className="project_description">
-                                        <p>
-                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sed tellus venenatis, luctus leo sit amet, mollis risus. Aenean elementum quis nunc eu auctor. Vivamus elementum sapien a lobortis rhoncus.
-                                        </p>
-                                    </div>
-                                    <div className="project_readmore">
-                                        <Link className="oxfam_button" href="#" >Learn more</Link>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="projects_item">
-                                <div className="projects_img">
-                                    <div className="project_cpt_image"
-                                        style={{"background": "url(/assets/images/OGB_111967_Elizabeth-and-her-son.jpg) center center no-repeat;background-size:cover"}}>
-                                    </div>
-                                </div>
-                                <div className="projects_text">
-                                    <h3>
-                                        Health
-                                    </h3>
-                                    <div className="project_description">
-                                        <p>
-                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sed tellus venenatis, luctus leo sit amet, mollis risus. Aenean elementum quis nunc eu auctor. Vivamus elementum sapien a lobortis rhoncus.
-                                        </p>
-                                    </div>
-                                    <div className="project_readmore">
-                                        <Link className="oxfam_button" href="#" >Learn more</Link>
-                                    </div>
-                                </div>
-                            </div>
+                                );
+                            })}
                         </div>
                     </div>
                 </div>
